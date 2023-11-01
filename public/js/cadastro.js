4cd5cd7dc171c9f54715e53cfb8d4ea816596593
Gabriel Amaral <gabriel.amaral@sptech.school>
2023-11-01 01:21:23 -0300

    Alterei a tela de cadastro

--- public/js/cadastro.js
+++ public/js/cadastro.js
@@ -1,16 +1,18 @@
-const frase1Texto = "Olá, bem-vindo ao fabuloso mundo Pokémon."
+const frase1Texto = "Olá, bem-vindo ao fabuloso mundo Pokémon.";
 const frase2Texto = "Meu nome é Carvalho mas todos aqui me chamam de professor Carvalho.";
 const frase3Texto = "Este mundo é habitado por criaturas chamadas de Pokémon,";
-const frase4Texto = "algumas pessoas os tratam como mascotes, outras os usam em batalhas."
-const frase5Texto = "Quanto a mim, eu amo estudar os Pokémon."
-const frase6Texto = "Mas primeiro, fale um pouco sobre você."
-let i = 0
+const frase4Texto = "algumas pessoas os tratam como mascotes, outras os usam em batalhas.";
+const frase5Texto = "Quanto a mim, eu amo estudar os Pokémon.";
+const frase6Texto = "Mas primeiro, fale um pouco sobre você.";
+let i = 0;
 let i2 = 0;
 let i3 = 0;
 let i4 = 0;
 let i5 = 0;
 let i6 = 0;
+
 window.addEventListener("load", function inserirTexto() {
+
     if (i < frase1Texto.length) {
         frase1.innerHTML += frase1Texto[i];
         i++;
@@ -22,13 +24,13 @@ window.addEventListener("load", function inserirTexto() {
         i3++;
     } else if (i4 < frase4Texto.length) {
         frase4.innerHTML += frase4Texto[i4];
-        i4++
+        i4++;
     } else if (i5 < frase5Texto.length) {
         frase5.innerHTML += frase5Texto[i5];
-        i5++
+        i5++;
     } else if (i6 < frase6Texto.length) {
         frase6.innerHTML += frase6Texto[i6];
-        i6++
+        i6++;
     } else {
         botao_cadastro.style.display = "flex";
     }
@@ -37,6 +39,29 @@ window.addEventListener("load", function inserirTexto() {
     setTimeout(inserirTexto, 25);
 })
 
+const cadastro = document.getElementById("botao_cadastro");
+const falaDialogo = document.getElementById("caixa_dialogo");
+const professor = document.getElementById("img_professor");
+const caixaCadastro = document.getElementById("caixa_cadastro");
+
+cadastro.addEventListener("click", () => {
+    falaDialogo.style.display = "none";
+    professor.style.display = "none";
+    caixaCadastro.style.display = "flex"
+})
+
+const formulario = document.getElementById("fomulario_cadastro");
+const senha = document.getElementById("senha_cadastro")
+const confirmaSenha = document.getElementById("confirma_senha");
+const cadastrar = document.getElementById("botao_cadastrar");
+const msgErro = document.getElementById("mensagem_erro_senha")
+
+formulario.addEventListener("submit", (batata) => {
+    if (senha.value!=confirmaSenha.value) {
+        batata.preventDefault();
+        msgErro.style.display = "flex";
+    }
+})
 
 
 
